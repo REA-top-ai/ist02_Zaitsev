2385b017b1058bf6546d82c3139daeb187c531b4
vasilq00 <Zaitsev112233@yandex.ru>
2026-05-30 20:31:23 +0300

    Delete PythonProject12 directory

--- PythonProject12/client/__init__.py
+++
@@ -1,9 +0,0 @@
-from .api_methods import get_top_headlines, get_everything, get_sources
-
-__all__ = [
-    'get_top_headlines',
-    'get_everything',
-    'get_sources'
-]
-
-__version__ = '1.0.0'
